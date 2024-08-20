f5edc00d612fad868da9592607dac675c5452f82
MiqG <miquelangladagirotto@gmail.com>
2024-08-20 12:32:51 +0200

    added figures PPI shortest paths STRINGDB

--- workflows/program_regulation/scripts/figures_upstream_regulators.R
+++ workflows/program_regulation/scripts/figures_upstream_regulators.R
@@ -64,6 +64,10 @@ PAL_DRIVER_TYPE = c(
 # event_info_file = file.path(RAW_DIR,'VastDB','EVENT_INFO-hg38_noseqs.tsv')
 # splicing_factors_file = file.path(SUPPORT_DIR,"supplementary_tables","splicing_factors.tsv")
 
+# shortest_paths_pert_sfs_file = file.path(RESULTS_DIR,'files','ppi','shortest_path_lengths_to_pert_splicing_factors.tsv.gz')
+# shortest_paths_random_file = file.path(RESULTS_DIR,'files','ppi','shortest_path_lengths_to_pert_splicing_factors_random.tsv.gz')
+
+
 ##### FUNCTIONS #####
 load_ontologies = function(msigdb_dir, cosmic_genes_file){
     ontologies = list(
@@ -99,6 +103,17 @@ plot_program_activity = function(cancer_program_activity){
             id_cols=c("PERT_ENSEMBL","PERT_GENE","cosmic_driver_type","target_in_cosmic","driver_type"), 
             names_from="activity_type", values_from="activity_diff"
         )
+    
+    plts[["program_activity-diff_vs_pert_efficiency-scatter"]] = X %>%
+        filter(activity_type=="activity_rpe1") %>%
+        drop_na(pert_efficiency_fc, activity_diff) %>%
+        ggplot(aes(x=pert_efficiency_fc, y=activity_diff)) +
+        geom_scattermore(pixels = c(1000,1000), pointsize=8, alpha=0.5, color=PAL_DARK) +
+        stat_cor(method="pearson", size=FONT_SIZE, family=FONT_FAMILY) + 
+        theme_pubr() +
+        theme(aspect.ratio=1) +
+        labs(x="KD Efficiency", y="Oncogenic vs Tumor Suppressor\nSplicing Program Activity")
+        
     
     plts[["program_activity-ts_vs_onco_by_cell_line-scatter"]] = X %>%
         ggplot(aes(x=`Tumor suppressor`, y=`Oncogenic`)) +
@@ -221,6 +236,38 @@ plot_program_activity = function(cancer_program_activity){
             size=FONT_SIZE,family=FONT_FAMILY, segment.size=0.1, max.overlaps=50
         ) +
         facet_wrap(~driver_type+activity_type, ncol=2) +
+        theme(aspect.ratio=1, strip.text.x = element_text(size=6, family=FONT_FAMILY)) +
+        labs(x="Relative Ranking", y="Oncogenic vs Tumor Suppressor\nSplicing Program Activity")
+    
+    plts[["program_activity-diff_ranking_by_cell_line_and_sfornot-scatter"]] = X %>%
+        drop_na(activity_diff) %>%
+        group_by(activity_type) %>%
+        arrange(-activity_diff) %>%
+        mutate(
+            ranking = row_number(),
+            rel_ranking = ranking / n()
+        ) %>%
+        ungroup() %>%
+        ggplot(aes(x=rel_ranking, y=activity_diff)) +
+        geom_scattermore(pixels = c(1000,1000), pointsize=8, alpha=0.5, color=PAL_DARK) +
+        theme_pubr() + 
+        geom_text_repel(
+            aes(label=PERT_GENE),
+            . %>% 
+            group_by(activity_type, pert_is_sf) %>% 
+            slice_max(activity_diff, n=5) %>% 
+            ungroup(),
+            size=FONT_SIZE, family=FONT_FAMILY, segment.size=0.1, max.overlaps=50
+        ) +
+        geom_text_repel(
+            aes(label=PERT_GENE),
+            . %>% 
+            group_by(activity_type, pert_is_sf) %>% 
+            slice_min(activity_diff, n=5) %>% 
+            ungroup(),
+            size=FONT_SIZE,family=FONT_FAMILY, segment.size=0.1, max.overlaps=50
+        ) +
+        facet_wrap(~pert_is_sf+activity_type, ncol=2) +
         theme(aspect.ratio=1, strip.text.x = element_text(size=6, family=FONT_FAMILY)) +
         labs(x="Relative Ranking", y="Oncogenic vs Tumor Suppressor\nSplicing Program Activity")
     
@@ -371,23 +418,48 @@ plot_enrichments = function(enrichments){
             low=PAL_FDR_LIGHT, high=PAL_FDR_DARK, 
             name="FDR", guide=guide_colorbar(reverse=TRUE))
     
+    ## activity difference without SFs
+    X = enrichments[["rpe1_activity_diff_gobp_nosf"]] %>%
+        as.data.frame() %>%
+        slice_max(NES, n=5) %>%
+        bind_rows(
+            enrichments[["rpe1_activity_diff_gobp_nosf"]] %>%
+                as.data.frame() %>%
+                slice_min(NES, n=5)
+        )
+        
+    plts[["enrichments-activity_diff_nosf-dot"]] = X %>%
+        mutate(
+            Count = str_count(core_enrichment, "/")+1, 
+            GeneRatio=Count/setSize
+        ) %>%
+        arrange(NES, Description) %>%
+        mutate(Description = factor(Description, levels=unique(Description))) %>%
+        ggscatter(x="NES", y="Description", size="GeneRatio", color="p.adjust") +
+        scale_size(range=c(0.5,3)) + 
+        scale_color_continuous(
+            low=PAL_FDR_LIGHT, high=PAL_FDR_DARK, 
+            name="FDR", guide=guide_colorbar(reverse=TRUE))    
+    
     X = enrichments
     for (x in names(X)){
         plt_name = sprintf("enrichments-activity_diff-%s-dot", x)
         df = X[[x]] %>% as.data.frame()
-        plts[[plt_name]] = df %>% 
-            slice_max(NES, n=5) %>%
-            bind_rows(
-                df %>% 
-                slice_min(NES, n=5)
-            ) %>%
-            plot_gsea()
+        if (nrow(df) > 0){
+            plts[[plt_name]] = df %>% 
+                slice_max(NES, n=5) %>%
+                bind_rows(
+                    df %>% 
+                    slice_min(NES, n=5)
+                ) %>%
+                plot_gsea()
+        }
     }
             
     return(plts)
 }
 
-plot_network_analysis = function(networks_sf_ex){
+plot_sf_network_analysis = function(networks_sf_ex){
     plts = list()
     
     X = networks_sf_ex
@@ -404,7 +476,7 @@ plot_network_analysis = function(networks_sf_ex){
         ungroup() %>%
         filter(n_total>=25)
     
-    plts[["network_analysis-target_type_freq_vs_activity_diff-scatter"]] = x %>%
+    plts[["sf_network_analysis-target_type_freq_vs_activity_diff-scatter"]] = x %>%
         drop_na(perc, activity_diff) %>%
         ggscatter(x="perc", y="activity_diff", size=1, alpha=0.5, color="target_type") +
         color_palette(as.vector(c("darkred","darkgreen",PAL_DRIVER_TYPE))) +
@@ -436,18 +508,42 @@ plot_network_analysis = function(networks_sf_ex){
     return(plts)
 }
 
-make_plots = function(cancer_program_activity, enrichments){
+
+plot_ppi_network_analysis = function(shortest_paths_pert_sfs){
+    plts = list()
+    
+    X = shortest_paths_pert_sfs %>%
+        filter(source!=target) %>%
+        drop_na()
+    
+    comparisons = list(c("Both Weak", "Both Strong"),c("Mixed", "Both Strong"),c("Mixed","Both Weak"))
+    plts[["ppi_network_analysis-pair_type_vs_path_length-violin"]] = X %>%
+        ggviolin(x="pair_type", y="shortest_path_length", trim=TRUE, fill="darkkhaki", color=NA, 
+                 add="median_iqr", add.params=list(color="black", size=0.1)) +
+        stat_compare_means(comparisons=comparisons, method="wilcox.test", size=FONT_SIZE, family=FONT_FAMILY) +
+        geom_text(
+            aes(y=0.1, label=label),
+            . %>% count(pair_type) %>% mutate(label=sprintf("n=%s",n)),
+            size=FONT_SIZE, family=FONT_FAMILY
+        ) +
+        labs(x="SF Pair", y="STRINGDB Shortest Path Length")
+    
+    return(plts)
+}
+
+make_plots = function(cancer_program_activity, enrichments, networks_sf_ex, shortest_paths_pert_sfs){
     plts = list(
         plot_program_activity(cancer_program_activity),
         plot_enrichments(enrichments),
-        plot_network_analysis(networks_sf_ex)
+        plot_sf_network_analysis(networks_sf_ex),
+        plot_ppi_network_analysis(shortest_paths_pert_sfs)
     )
     plts = do.call(c,plts)
     return(plts)
 }
 
 
-make_figdata = function(cancer_program_activity, enrichments){
+make_figdata = function(cancer_program_activity, enrichments,  networks_sf_ex, shortest_paths_pert_sfs){
     figdata = list(
         "eval_bulk_vs_singlecell" = list(
             "cancer_program_activity" = cancer_program_activity
@@ -471,20 +567,24 @@ save_plt = function(plts, plt_name, extension='.pdf',
     save_plot(filename, plt, base_width=width, base_height=height, dpi=dpi, units='cm')
 }
 
-
 save_plots = function(plts, figs_dir){
+    save_plt(plts, "program_activity-diff_vs_pert_efficiency-scatter", '.pdf', figs_dir, width=5, height=5)
     save_plt(plts, "program_activity-ts_vs_onco_by_cell_line-scatter", '.pdf', figs_dir, width=6, height=4)
     save_plt(plts, "program_activity-diff_ranking_overview-scatter", '.pdf', figs_dir, width=8, height=6)
     save_plt(plts, "program_activity-diff_ranking_by_cell_line_and_cosmic-scatter", '.pdf', figs_dir, width=8, height=17)
     save_plt(plts, "program_activity-diff_ranking_by_cell_line_and_program-scatter", '.pdf', figs_dir, width=8, height=17)
+    save_plt(plts, "program_activity-diff_ranking_by_cell_line_and_sfornot-scatter", '.pdf', figs_dir, width=8, height=12)
     save_plt(plts, "program_activity-diff_rpe1_vs_k562-scatter", '.pdf', figs_dir, width=8, height=8)
     save_plt(plts, "program_activity-diff_vs_cosmic-violin", '.pdf', figs_dir, width=8, height=8)
     save_plt(plts, "program_activity-diff_vs_splicing_program-violin", '.pdf', figs_dir, width=8, height=8)
     
     save_plt(plts, "enrichments-activity_programs-dot", '.pdf', figs_dir, width=12, height=7.5)
     save_plt(plts, "enrichments-activity_diff-dot", '.pdf', figs_dir, width=11, height=5.5)
+    save_plt(plts, "enrichments-activity_diff_nosf-dot", '.pdf', figs_dir, width=12.5, height=5.5)
     
-    save_plt(plts, "network_analysis-target_type_freq_vs_activity_diff-scatter", '.pdf', figs_dir, width=8, height=9)
+    save_plt(plts, "sf_network_analysis-target_type_freq_vs_activity_diff-scatter", '.pdf', figs_dir, width=8, height=9)
+    
+    save_plt(plts, "ppi_network_analysis-pair_type_vs_path_length-violin", '.pdf', figs_dir, width=5, height=6)
 }
 
 
@@ -501,7 +601,6 @@ save_figdata = function(figdata, dir){
         })
     })
 }
-
 
 parseargs = function(){
     
@@ -548,6 +647,9 @@ main = function(){
     event_info = read_tsv(event_info_file)
     splicing_factors = read_tsv(splicing_factors_file)
     
+    shortest_paths_pert_sfs = read_tsv(shortest_paths_pert_sfs_file)
+    shortest_paths_random = read_tsv(shortest_paths_random_file)
+    
     # prep
     gene_info = gene_info %>%
         mutate(PERT_GENE = `Approved symbol`) %>%
@@ -589,9 +691,19 @@ main = function(){
         left_join(
             cancer_program %>% distinct(GENE, ENSEMBL, driver_type), 
             by=c("PERT_ENSEMBL"="ENSEMBL", "PERT_GENE"="GENE")
+        ) %>%
+        left_join(
+            metadata_rpe1 %>%
+                distinct(PERT_ENSEMBL, n_cells, pert_efficiency_fc) %>%
+                mutate(activity_type = "activity_rpe1"),
+            by = c("activity_type","PERT_ENSEMBL")
+        ) %>%
+        mutate(
+            pert_is_sf = PERT_ENSEMBL %in% splicing_factors[["ENSEMBL"]]
         )
     
     # enrichments
+    ## full
     enrichments = list()
     genes = cancer_program_activity %>% 
         filter(activity_type=="activity_rpe1") %>% 
@@ -615,8 +727,17 @@ main = function(){
     enrichments[["rpe1_activity_diff_gobp"]] = GSEA(geneList = genes, TERM2GENE=ontologies[["GO_BP"]])
     enrichments[["rpe1_activity_diff_reactome"]] = GSEA(geneList = genes, TERM2GENE=ontologies[["reactome"]])
     enrichments[["rpe1_activity_diff_chea"]] = GSEA(geneList = genes, TERM2GENE=ontologies[["CHEA"]])
+    ## without splicing factor perturbations
+    genes = cancer_program_activity %>% 
+        filter(activity_type=="activity_rpe1" & !pert_is_sf) %>% 
+        arrange(-activity_diff) %>% 
+        distinct(PERT_GENE,activity_diff) %>% 
+        deframe()
+    enrichments[["rpe1_activity_diff_gobp_nosf"]] = GSEA(geneList = genes, TERM2GENE=ontologies[["GO_BP"]])
+    enrichments[["rpe1_activity_diff_reactome_nosf"]] = GSEA(geneList = genes, TERM2GENE=ontologies[["reactome"]])
+    enrichments[["rpe1_activity_diff_chea_nosf"]] = GSEA(geneList = genes, TERM2GENE=ontologies[["CHEA"]])
     
-    # network analysis
+    # target exon network analysis
     networks_sf_ex = networks_sf_ex %>% 
         distinct(regulator, target) %>%
         left_join(
@@ -647,17 +768,46 @@ main = function(){
             by = c("PERT_GENE"="GENE", "regulator"="ENSEMBL")
         )
     
+    # SF PPI analysis
+    ## are SFs closer to each other that produce strong changes?
+    sf_switch_type = cancer_program_activity %>% 
+        filter(activity_type=="activity_rpe1" & pert_is_sf) %>% 
+        mutate(
+            switch_type = case_when(
+                activity_diff >= 2 ~ "strong",
+                activity_diff < 2 ~ "weak"
+            ) 
+        ) %>%
+        distinct(PERT_GENE, switch_type)
+    
+    shortest_paths_pert_sfs = shortest_paths_pert_sfs %>%
+        left_join(
+            sf_switch_type %>% rename(switch_type_source = switch_type),
+            by=c("source"="PERT_GENE")
+        ) %>%
+        left_join(
+            sf_switch_type %>% rename(switch_type_target = switch_type),
+            by=c("target"="PERT_GENE")
+        ) %>%
+        mutate(
+            pair_type = case_when(
+                switch_type_source=="weak" & switch_type_target=="weak" ~ "Both Weak",
+                switch_type_source=="weak" & switch_type_target=="strong" ~ "Mixed",
+                switch_type_source=="strong" & switch_type_target=="weak" ~ "Mixed",
+                switch_type_source=="strong" & switch_type_target=="strong" ~ "Both Strong"
+            )
+        )
+    
     # plot
-    plts = make_plots(cancer_program_activity, enrichments, networks_sf_ex)
+    plts = make_plots(cancer_program_activity, enrichments, networks_sf_ex, shortest_paths_pert_sfs)
     
     # make figdata
-    figdata = make_figdata(cancer_program_activity, enrichments, networks_sf_ex)
+    figdata = make_figdata(cancer_program_activity, enrichments, networks_sf_ex, shortest_paths_pert_sfs)
     
     # save
     save_plots(plts, figs_dir)
     save_figdata(figdata, figs_dir)
 }
-
 
 ##### SCRIPT #####
 if (sys.nframe() == 0L) {
